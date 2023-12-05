fecba31c579f2f934573e5ba6a6d9e4440a6ca5d
biel071 <144127906+biel071@users.noreply.github.com>
2023-12-05 10:52:52 -0300

    Add files via upload

---
+++ Fun01.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float somar(float n1, float n2)
+{
+    float resultado;
+    resultado = (n1 + n2) / 2;
+
+    return resultado;
+}
+int main()
+{
+    float num1, num2;
+    float media;
+    printf("Digite o primeiro numero:");
+    scanf("%f", &num1);
+
+    printf("Digite o segundo numero:");
+    scanf("%f", &num2);
+
+    media = somar (num1, num2);
+
+    printf("Primeiro numero:%.1f \n", num1);
+    printf("Segundo numero:%.1f \n", num2);
+    printf("Resultado da media: %.1f \n", media);
+    return 0;
+}
