513302ae4648a178da5eb62553a50c0e4bd4f07e
biel071 <144127906+biel071@users.noreply.github.com>
2023-12-05 11:29:03 -0300

    Add files via upload

---
+++ Fun02.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void tabuada (int n1){
+int i;
+for ( i = 1; i < 10; i++)
+{
+    printf ("%d x %d= %d \n",n1,i,n1*i);
+
+}
+}
+int main (){
+
+    int num;
+   
+    printf ("Digite um numero:");
+    scanf ("%d",&num);
+
+    tabuada(num);
+
+    
+}
